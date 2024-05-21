67bd6af6b6b26b048d50722383604b1490ce26ce
HappyNaCl <yudhistira.fadly@gmail.com>
2024-05-21 20:52:03 +0700

    'feat:recommendation'

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const navbar = document.getElementById('nav-bar');
 let stickyOffset = navbar.offsetTop;
 
 function stickNavbar(){
-    if(document.documentElement.scrollTop > 20){
+    if(document.documentElement.scrollTop > 0){
         navbar.classList.add('sticky');
     }else{
         navbar.classList.remove('sticky');
@@ -18,3 +18,30 @@ function stickNavbar(){
 }
 
 window.addEventListener('scroll', stickNavbar)
+
+//Scroll Animations
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const cardObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('card-show')
+        }else{
+            entry.target.classList.remove('card-show')
+        }
+    });
+})
+
+const hiddenElements = document.getElementsByClassName('hidden')
+Array.from(hiddenElements).forEach((el) => observer.observe(el))
+
+const cards = document.getElementsByClassName('card-hidden')
+Array.from(cards).forEach((el) => observer.observe(el))

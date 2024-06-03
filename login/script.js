4911189919b6faa240690f661f4d36b1b5eaa7f6
yoyo <yudhistira.fadly@gmail.com>
2024-06-03 21:22:27 +0700

    TODO: set to localstorage, add unique validation to reg, add login validation

--- login/script.js
+++ login/script.js
@@ -63,14 +63,111 @@ registerForm.addEventListener('submit', (e) => {
     e.preventDefault();
 })
 
+let errorMsg = ""
+
+function resetError(){
+    errorMsg = ""
+}
+
+function resetBday(){
+    document.getElementById('month-select').style = "border-bottom: var(--whitesmoke) solid 1px;";
+    document.getElementById('bday-year').style = "border-bottom: var(--whitesmoke) solid 1px;";
+    document.getElementById('bday-date').style = "border-bottom: var(--whitesmoke) solid 1px;";
+}
+
 function validateUsername(str){
     if(str.length < 8 || str.length > 20){
+        errorMsg = "Username must be between 8-20 characters"
         return false;
     }
     for(let i = 0; i < str.length; i++){
         if(!((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z') || (str[i] >= '0' && str[i] <= '9'))){
+            errorMsg = "Username must not contain any symbols"
             return false;
         }
+    }
+    return true;
+}
+
+function validatePassword(str){
+    if(str.length < 8 || str.length > 20){
+        errorMsg = "Password must be between 8-20 characters"
+        return false;
+    }
+    let haveNum = false, haveAlphabet = false;
+    for(let i = 0; i < str.length; i++){
+        if((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')){
+            haveAlphabet = true;
+        }else if(str[i] >= '0' && str[i] <= '9'){
+            haveNum = true;
+        }
+        else{
+            errorMsg = "Password must not contain any special characters"
+            return false;
+        }
+    }
+    if(!haveNum || !haveAlphabet){
+        errorMsg = "Password must be alphanumerical"
+        return false;
+    }
+    return true;
+}
+
+function validateEmail(str){
+    if(str.length < 0){
+        errorMsg = "Email must be filled"
+        return false;
+    }
+    if(str.indexOf('@') == -1){
+        errorMsg = "Email must have \'@\'"
+        return false;
+    }
+    if(!str.endsWith('.com')){
+        errorMsg = "Email must end with \.com"
+        return false;
+    }
+    let domainName = str.substring(str.indexOf('@') + 1, str.indexOf('.'));
+    if(domainName.length < 0){
+        errorMsg = "Please use a valid email"
+        return false;
+    }
+    return true;
+}
+
+function validateBday(date, month, year){
+    if(typeof(date) !== Number){
+        date = parseInt(date);
+    }
+    if(typeof(year) !== Number){
+        year = parseInt(year);
+    }
+    console.log(date, month, year);
+    if(month === "none"){
+        document.getElementById('month-select').style = "border-bottom: red solid 1px;";
+        errorMsg = "Please fill in your birth month";
+        return false;
+    }
+    
+    if((new Date().getUTCFullYear()- year) < 13){
+        document.getElementById('bday-year').style = "border-bottom: red solid 1px;";
+        errorMsg = "You must be 13 years or older to join KEYU Music"
+        return false;
+    }
+    const oddMonth = ['jan', 'march', 'may', 'jul', 'aug', 'oct', 'dec'];
+    let validDate = false;
+    if(oddMonth.includes(month) && (date >= 1 && date <= 31)){
+        validDate = true;
+    }else if(month === "feb" && (date >= 1 && date <= 29) && year % 4 == 0){
+        validDate = true;
+    }else if(month === "feb" && (date >= 1 && date <= 28) && year % 4 != 0){
+        validDate = true;
+    }else if(!oddMonth.includes(month) && (date >= 1 && date <= 30)){
+        validDate = true;
+    }
+    if(!validDate){
+        document.getElementById('bday-date').style = "border-bottom: red solid 1px;";
+        errorMsg = "Please fill in a valid date";
+        return false;
     }
     return true;
 }
@@ -81,16 +178,56 @@ function validateRegister(){
     const passwordVal = registerForm.elements['reg-password'].value;
     const conPasswordVal = registerForm.elements['reg-con-password'].value;
     const bdayDateVal = registerForm.elements['bday-date'].value;
-
+    const bdayMonthVal = registerForm.elements['bday-month'].value;
+    const bdayYearVal = registerForm.elements['bday-year'].value;
     let isValid = true;
 
     //Username must be 8 - 20 Characters, can't contain symbols
     if(!validateUsername(usernameVal)){
         isValid = false;
         registerForm.elements['reg-username'].parentElement.classList.add('error')
-        document.getElementById('r-username-err').innerText = "Username must be between 8 - 20 characters and does not contain any symbol"
+        document.getElementById('r-username-err').innerText = errorMsg;
+    }else{
+        registerForm.elements['reg-username'].parentElement.classList.remove('error')
+        document.getElementById('r-username-err').innerText = "";
     }
-
+    resetError();
+    //Email must contain '@', ends with '.com', domain name must be valid
+    if(!validateEmail(emailVal)){
+        isValid = false;
+        registerForm.elements['reg-email'].parentElement.classList.add('error')
+        document.getElementById('r-email-err').innerText = errorMsg;
+    }else{
+        registerForm.elements['reg-email'].parentElement.classList.remove('error')
+        document.getElementById('r-email-err').innerText = "";
+    }
+    resetError();
+    //Password must be 8-20 characters, alphanumerical
+    if(!validatePassword(passwordVal)){
+        isValid = false;
+        registerForm.elements['reg-password'].parentElement.classList.add('error')
+        document.getElementById('r-password-err').innerText = errorMsg;
+    }else{
+        registerForm.elements['reg-password'].parentElement.classList.remove('error')
+        document.getElementById('r-password-err').innerText = "";
+    }
+    resetError();
+    if(conPasswordVal !== passwordVal){
+        isValid = false;
+        registerForm.elements['reg-con-password'].parentElement.classList.add('error')
+        errorMsg = "Confirm Password needs to match password"
+        document.getElementById('r-con-password-err').innerText = errorMsg;
+    }else{
+        registerForm.elements['reg-con-password'].parentElement.classList.remove('error')
+        document.getElementById('r-con-password-err').innerText = "";
+    }
+    resetError();
+    if(!validateBday(bdayDateVal, bdayMonthVal, bdayYearVal)){
+        isValid = false;
+        document.getElementById('r-birthday-err').innerText = errorMsg;
+    }else{
+        resetBday();
+        document.getElementById('r-birthday-err').innerText = "";
+    }
+    resetError();
 }
-
-console.log(validateUsername(''));

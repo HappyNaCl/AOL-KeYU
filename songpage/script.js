c5eda4b5db3c3e8495685f5f7f2ba6906d5e43f5
HappyNaCl <yudhistira.fadly@gmail.com>
2024-06-13 21:31:14 +0700

    FINISHHH

--- songpage/script.js
+++ songpage/script.js
@@ -18,3 +18,36 @@ function stickNavbar(){
 }
 
 window.addEventListener('scroll', stickNavbar)
+
+//Full Carousel
+const fullPrev = document.getElementsByClassName('full-prev');
+const fullNext = document.getElementsByClassName('full-next');
+const fullCarousels = document.getElementsByClassName('full-carousel');
+const fullTracks = document.getElementsByClassName('full-track');
+
+let fullCarouselWidth = [];
+let fullCarouselIndex = [];
+for(let i = 0; i < fullCarousels.length; i++){
+    fullCarouselWidth.append(fullCarousels[i].offsetWidth);
+    fullCarouselIndex.append(0);
+}
+
+window.addEventListener('resize', ()=>{
+    for(let i = 0; i < fullCarousels.length; i++){
+        fullCarouselWidth[i] = fullCarousels[i].offsetWidth;
+    }
+})
+
+for(let i = 0; i < fullCarousels.length; i++){
+    fullNext[i].addEventListener('click', (e) => {
+        e.preventDefault();
+        setTimeout(function(){}, 1000);
+        index++;
+        fullPrev[i].classList.add('show');
+        fullTracks[i].style.transform = `translateX(${ fullCarouselIndex[i] * (-fullCarouselWidth[i]) }px)`
+        if(fullTracks[i] - (fullCarouselIndex[i] * fullCarouselWidth[i]) <  (fullCarouselIndex[i] * fullCarouselWidth[i])){
+            fullNext[i].classList.add('hide');
+        }
+    })
+}
+
